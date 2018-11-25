ebcc534b17ccd61797b9f9acb79d2f6d6fe3246e
malgorzata.majchrowi <maugosiam@gmail.com>
2018-11-25 21:43:47 +0100

    ...

--- src/FullRights.java
+++ src/FullRights.java
@@ -4,23 +4,4 @@ public class FullRights extends CanModify {
     public FullRights(String firstNam, String lastName, int id, String address) {
         super(firstNam, lastName, id, address);
     }
-
-//
-//    @Override
-//    public void createFile(){
-//        System.out.println("You have already created file");
-//        new WordDoc();
-//    }
-//
-//    @Override
-//    public void modifyFile(){
-//        System.out.println("You have already modified file");
-//    }
-//
-//    @Override
-//    public void deleteFile() {
-//        System.out.println("You have already deleted file");
-//    }
-//
-
 }
