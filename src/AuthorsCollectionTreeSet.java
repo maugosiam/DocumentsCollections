bef1652528ed39fedb1ff8cb2a53a1bc04aab4aa
malgorzata.majchrowi <maugosiam@gmail.com>
2018-12-02 20:53:34 +0100

    ...

---
+++ src/AuthorsCollectionTreeSet.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class AuthorsCollectionTreeSet {
+
+    TreeSet<Author> authorTreeSet = new TreeSet<>();
+
+//for some reason it doeasnt work. to clarify!
+/*@Override
+public String toString() {
+    return  "TreeSet";
+}*/
+}
--- src/Program.java
+++ src/Program.java
@@ -4,10 +4,11 @@ public class Program {
 
     public static void main (String[] args){
 
-        AuthorsCollection authors = new AuthorsCollection();
+        AuthorsCollectionArrayList authors = new AuthorsCollectionArrayList();
         authors.authorArrayList.add(new ReadOnly("Adam", "Badowski", 1, "ul.Sezamkowa 14, Warszawa"));
         authors.authorArrayList.add(new CanModify("Cezary", "Duda", 2, "266 Broadway Str, New York"));
         authors.authorArrayList.add(new FullRights("Edyta", "Fijałkiewicz", 3, "ul.Kraciasta 23/5, Słupsk"));
+
 
         for (int i = 0; i<authors.authorArrayList.size();i++) {
             System.out.println("ELEMENT"+i+": "+authors.authorArrayList.get(i));
@@ -26,5 +27,14 @@ public class Program {
         System.out.println(authors.authorArrayList.get(2).getLastName()+" created file: "+documents[2].getDocName()+" having hashcode: "+documents[2].hashCode());
 
         ((WordDoc) documents[0]).druk();
+
+        AuthorsCollectionTreeSet authorTS = new AuthorsCollectionTreeSet();
+        /*authorTS.authorTreeSet.add(new ReadOnly("Adam", "Badowski", 1, "ul.Sezamkowa 14, Warszawa"));
+        authorTS.authorTreeSet.add(new CanModify("Cezary", "Duda", 2, "266 Broadway Str, New York"));
+        authorTS.authorTreeSet.add(new FullRights("Edyta", "Fijałkiewicz", 3, "ul.Kraciasta 23/5, Słupsk"));
+       */ //System.out.println(authorTS.authorTreeSet);
+
+
+
     }
 }
