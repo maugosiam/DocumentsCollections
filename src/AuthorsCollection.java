50a0af6e5709c4245fdadf0be1b4f65905dc2eb2
malgorzata.majchrowi <maugosiam@gmail.com>
2018-12-02 17:07:26 +0100

    ...

---
+++ src/AuthorsCollection.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class AuthorsCollection {
+    protected ArrayList <Author> authorArrayList = new ArrayList<>();
+}
--- src/Program.java
+++ src/Program.java
@@ -18,26 +18,27 @@ public class Program {
 
     public static void main (String[] args){
 
-        ArrayList<Author> authors = new ArrayList();
-        authors.add(new ReadOnly("Adam", "Badowski", 1, "ul.Sezamkowa 14, Warszawa"));
-        authors.add(new CanModify("Cezary", "Duda", 2, "266 Broadway Str, New York"));
-        authors.add(new FullRights("Edyta", "Fijałkiewicz", 3, "ul.Kraciasta 23/5, Słupsk"));
+        //ArrayList<Author> authors = new ArrayList();
+        AuthorsCollection authors = new AuthorsCollection();
+        authors.authorArrayList.add(new ReadOnly("Adam", "Badowski", 1, "ul.Sezamkowa 14, Warszawa"));
+        authors.authorArrayList.add(new CanModify("Cezary", "Duda", 2, "266 Broadway Str, New York"));
+        authors.authorArrayList.add(new FullRights("Edyta", "Fijałkiewicz", 3, "ul.Kraciasta 23/5, Słupsk"));
 
-        for (int i = 0; i<authors.size();i++) {
-            System.out.println("ELEMENT"+i+": "+authors.get(i));
+        for (int i = 0; i<authors.authorArrayList.size();i++) {
+            System.out.println("ELEMENT"+i+": "+authors.authorArrayList.get(i));
         }
 
         Documents[] documents = {
-                authors.get(0).createWordDoc("tra la la la.doc", "Description of Word Document", 100),
-                authors.get(1).createPictureDoc("picture of the elephant.jpg", "Description of Picture document", 700, 600, 900),
-                authors.get(2).createVideoDoc("scary movie.mp4", "Description of Video document", 900, 80000, 900, 60)
+                authors.authorArrayList.get(0).createWordDoc("tra la la la.doc", "Description of Word Document", 100),
+                authors.authorArrayList.get(1).createPictureDoc("picture of the elephant.jpg", "Description of Picture document", 700, 600, 900),
+                authors.authorArrayList.get(2).createVideoDoc("scary movie.mp4", "Description of Video document", 900, 80000, 900, 60)
         };
 
         ((WordDoc) documents[0]).druk();
 
-        System.out.println(authors.get(0).getLastName()+" created file: "+documents[0].getDocName()+" having hashcode: "+documents[0].hashCode());
-        System.out.println(authors.get(1).getLastName()+" created file: "+documents[1].getDocName()+" having hashcode: "+documents[1].hashCode());
-        System.out.println(authors.get(2).getLastName()+" created file: "+documents[2].getDocName()+" having hashcode: "+documents[2].hashCode());
+        System.out.println(authors.authorArrayList.get(0).getLastName()+" created file: "+documents[0].getDocName()+" having hashcode: "+documents[0].hashCode());
+        System.out.println(authors.authorArrayList.get(1).getLastName()+" created file: "+documents[1].getDocName()+" having hashcode: "+documents[1].hashCode());
+        System.out.println(authors.authorArrayList.get(2).getLastName()+" created file: "+documents[2].getDocName()+" having hashcode: "+documents[2].hashCode());
 
         ((WordDoc) documents[0]).druk();
     }
